a74d4029761561dc991008e227e378535deaf80c
wesely1996 <nikola.wesely.96@gmail.com>
2022-10-06 00:11:30 +0200

    Static files

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.set('view engine', 'ejs');
 
 app.listen(3000);
 
+//middleware & static files
+
+app.use((req, res, next) => {
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    next();
+});
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     const blogs = [
         {
