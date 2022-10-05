323f520d74538961d5066bff915f93f4e88ac63e
wesely1996 <nikola.wesely.96@gmail.com>
2022-10-05 20:12:03 +0200

    Switch pages and Set statusCodes

--- server.js
+++ server.js
@@ -6,7 +6,23 @@ const server = http.createServer((req,res)=>{
 
     res.setHeader('Content-Text','text/html')
 
-    fs.readFile("./views/index.html", (err, data) =>{
+    let path = './views/'
+
+    switch(req.url){
+        case '/':
+            path += 'index.html'
+            res.statusCode = 200
+            break
+        case '/about':
+            path += 'about.html'
+            res.statusCode = 200
+            break
+        default:
+            path += '404.html'
+            res.statusCode = 404
+    }
+
+    fs.readFile(path, (err, data) =>{
         if(err){
             console.log(err);
         }
